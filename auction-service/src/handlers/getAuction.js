59a2644a5de44fd22df55ae6b149100a75eec857
Vishwas Puri <vishwaspuri09@gmail.com>
2020-07-24 12:36:58 +0530

    Basic CRUD functionality implemented

---
+++ auction-service/src/handlers/getAuction.js
@@ -0,0 +1,38 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk'
+import middy from '@middy/core'
+import httpJsonBodyParser from '@middy/http-json-body-parser'
+import httpEventNormalizer from '@middy/http-event-normalizer'
+import httpErrorHandler from '@middy/http-error-handler'
+import createError from 'http-errors'
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function getAuction(event, context) {
+    let auction;
+    const { id } = event.pathParameters;
+
+    try{
+        const result = await dynamodb.get({
+            TableName:  process.env.AUCTIONS_TABLE_NAME,
+            Key:    { id }
+        }).promise();
+        auction = result.Item;
+    }   catch(e)    {
+        console.error(e);
+        throw new createError.InternalServerError(e);
+    }
+    if (!auction){
+        throw new createError.NotFound(`Auction with ID "${id}" not found!`)
+    }
+    return {
+        statusCode: 200, 
+        body: JSON.stringify(auction),
+    };
+}
+
+export const handler = middy(getAuction)
+  .use(httpJsonBodyParser()) // Automatically parses event/ context so that we don't have to use JSON parser
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler());
+  
---
+++ auction-service/src/handlers/getAuctions.js
@@ -0,0 +1,34 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk'
+import middy from '@middy/core'
+import httpJsonBodyParser from '@middy/http-json-body-parser'
+import httpEventNormalizer from '@middy/http-event-normalizer'
+import httpErrorHandler from '@middy/http-error-handler'
+import createError from 'http-errors'
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function getAuctions(event, context) {
+  let auctions;
+
+  try{
+    const result = await dynamodb.scan({
+        TableName: process.env.AUCTIONS_TABLE_NAME
+    }).promise();
+    auctions = (await result).Items
+  } catch(error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  return {
+    statusCode: 200, 
+    body: JSON.stringify(auctions),
+  };
+}
+
+export const handler = middy(getAuctions)
+  .use(httpJsonBodyParser()) // Automatically parses event/ context so that we don't have to use JSON parser
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler());
+  
